b08c0dca114cf2589e348aaa52f544ab9cfce14d
lucianoBrd <lucien.burdet@gmail.com>
2020-02-11 16:35:29 +0100

    find default position

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,30 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+jQuery(function ($) {
 
-var url = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}';
+    /* Default Lyon position */
+    var lat = 45.75;
+    var lng = 4.85;
+    var url = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}';
 
-L.tileLayer(url, {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    accessToken: token
-}).addTo(mymap);
+    /* Create the map */
+    var map = L.map('mapid').setView([lat, lng], 5);
+
+    /* Load the map */
+    L.tileLayer(url, {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        accessToken: token
+    }).addTo(map);
+
+    /* Locate the user */
+    map.locate({ setView: true, watch: true })
+        .on('locationfound', function (e) {
+            lat = e.latitude;
+            lng = e.longitude;
+        })
+        .on('locationerror', function (e) {
+            
+        });
+
+
+});
