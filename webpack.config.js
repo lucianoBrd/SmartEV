01f3e98caf5cc1bbfec4b721f63eb9b3e692f61f
Redwan <redwan@integral-service.fr>
2020-02-12 14:45:04 +0100

    add fonts package

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require("webpack");
 const path = require("path");
 const ExtractTextWebpackPlugin = require("extract-text-webpack-plugin");
+const GoogleFontsPlugin = require('google-fonts-plugin')
 
 let config = {
     entry: "./src/index.js",
@@ -19,7 +20,8 @@ let config = {
             }]
     },
     plugins: [
-        new ExtractTextWebpackPlugin("styles.css")
+        new ExtractTextWebpackPlugin("styles.css"),
+        new GoogleFontsPlugin('fonts-config.json')
     ]
 };
 module.exports = config;
