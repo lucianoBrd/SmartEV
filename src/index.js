24a3848051d6f2f63ee6cf9bf2a37c237f689d41
lucianoBrd <lucien.burdet@gmail.com>
2020-02-12 10:56:53 +0100

    create maps js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ require("../assets/stylesheets/home.scss");
 
 import './js/token';
 import './lib/leaflet/leaflet';
-import './js/variables'
+import './js/variables';
+import './js/maps';
 import './js/main';
