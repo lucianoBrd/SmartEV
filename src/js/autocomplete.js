aa1c0ad47fa4340e8781d0cf457b5be2a0fbd131
lucianoBrd <lucien.burdet@gmail.com>
2020-04-12 17:15:53 +0200

    loader autocomplete

--- src/js/autocomplete.js
+++ src/js/autocomplete.js
@@ -5,6 +5,7 @@ autocomplete = function () {
     var $input = $('.input-location');
     var list;
     var val;
+    var $currentInput;
 
     /* On keyup, start the countdown */
     $input.on('keyup', function () {
@@ -12,6 +13,7 @@ autocomplete = function () {
 
         list = $(this).attr('list');
         val = $(this).val();
+        $currentInput = $(this);
 
         typingTimer = setTimeout(doneTyping, doneTypingInterval);
     });
@@ -21,16 +23,29 @@ autocomplete = function () {
         clearTimeout(typingTimer);
     });
 
-    function doneTyping () {
+    function doneTyping() {
         if (!isEmpty(val)) {
+            /* Get label linked to the input */
+            var $label = $("label[for='" + $currentInput.attr('id') + "']");
+            var iconClass   = null,
+                iconPrefix  = null;
+            if ($label.length > 0) {
+                var $icon = $($label[0]).find('svg');
+                if ($icon.length > 0) {
+                    /* Save the class of the icon */
+                    iconClass   = $icon.attr('data-icon');
+                    iconPrefix  = $icon.attr('data-prefix');
+                    /* Replace icon with loader */
+                    changeIcon($label, true, iconClass, iconPrefix);
+                }
+            }
 
             $.ajax({
                 url: URLS['searchGeocoding'] + locationIQToken + "&limit=5&format=json&q=" + val
             }).done(function (data) {
-
                 /* Get the datalist */
                 var datalist = $('#' + list);
-                
+
                 /* Remove all child */
                 $(datalist).empty();
 
@@ -49,10 +64,14 @@ autocomplete = function () {
                     if (i == 0) {
                         /* Save the lat and lng */
                         $(document).trigger('load-' + list.split('-')[0], item);
-                    } 
+                    }
                 });
+
+                /* Remove loader */
+                changeIcon($label, false, iconClass, iconPrefix);
             }).fail(function (data) {
-                console.log(data);
+                /* Remove loader */
+                changeIcon($label, false, iconClass, iconPrefix);
             });
         }
     }
@@ -60,5 +79,31 @@ autocomplete = function () {
     /* Check if string not empty or blank or contains only white-space */
     function isEmpty(str) {
         return (!str || 0 === str.length || /^\s*$/.test(str) || !str.trim());
+    }
+
+    /**
+     * Change the icon of a label
+     * If loader is true, put a loader icon animation
+     * Else put the old icon
+     * @param {*} $label 
+     * @param {*} loader 
+     * @param {*} iconClass 
+     * @param {*} iconPrefix 
+     */
+    function changeIcon(
+        $label, 
+        loader, 
+        iconClass, 
+        iconPrefix
+    ) {
+        if (loader) {
+            $label.empty();
+            $label.append('<i class="fas fa-circle-notch fa-spin"></i>');
+        } else {
+            if (iconClass) {
+                $label.empty();
+                $label.append('<i class="' + iconPrefix + ' fa-' + iconClass + '"></i>');
+            }
+        }
     }
 };
